036e34265c69238a8d6847573dac2da1b653f1c6
Debi Prasad Rath <debi.rath817@gmail.com>
2023-04-06 22:32:52 +0530

    fixed issues on copy data from archive to input

--- README.md
+++ README.md
@@ -17,3 +17,19 @@ version 0.0.1:-
         2.3- if merged data present then read as is
     4- load data file from base path
     5- runnable data pipeline with data load(TEST success)
+
+
+version 0.0.2:-
+---------------
+
+    1- create a concept of data pipeline
+    2- add data access methods from filepath
+        2.1- check data file availability in base path
+            2.1.1- verify base path, if not create it
+        2.2- in case file not there, copy from archive
+            2.2.1- verify archive path, if not create it
+        2.3- if merged data present then read as is
+            2.3.1- merge data read need to be fixed [TODO]
+    4- load data file from base path
+    5- runnable data pipeline with data load(TEST success)
+
--- data_loader/load_data.py
+++ data_loader/load_data.py
@@ -11,23 +11,54 @@ class LoadData(Configuration):
     def __init__(self,BASE_PATH, ARCHIVE_PATH):
         super().__init__(BASE_PATH, ARCHIVE_PATH)
 
-    def read_csv_file_if_exists(self):
-        """reading CSV file if exist in a given path"""        
-        if Path(f"{self.BASE_PATH}/input").exists():
-            with os.scandir(f"{self.BASE_PATH}/input") as files_in_basepath:        
-                for item in files_in_basepath: 
-                    return True   
-        else:
-            return self.copy_file_from_archive() # copy files from archive folder
-    
-    def copy_file_from_archive(self):
-            if not self.read_csv_file_if_exists():
-                shutil.copy(self.ARCHIVE_PATH,f"{self.BASE_PATH}/input")
-                return True
-            else:
-                raise Exception("file not there")
+    # def read_csv_file_if_exists(self):
+    #     """reading CSV file if exist in a given path"""        
+    #     if Path(f"{self.BASE_PATH}/input").exists():
+    #         with os.scandir(f"{self.BASE_PATH}/input") as files_in_basepath:        
+    #             for item in files_in_basepath: 
+    #                 return True   
+    #     else:
+    #         return self.copy_file_from_archive() # copy files from archive folder
 
-    def get_source_data(self, item, feature_name, **kwargs):    
+    def read_csv_file_if_exists(self):
+        """reading CSV file if exist in a given path"""
+        if not os.path.exists(self.BASE_PATH+ "input"):
+            os.makedirs(self.BASE_PATH + "input")
+   
+        elif not os.path.exists(self.ARCHIVE_PATH):
+            os.makedirs(self.BASE_PATH + "archive")
+
+        elif Path(f"{self.BASE_PATH}/input").glob("*.csv"):
+            return True
+   
+        else:
+            return self.copy_file_from_archive(self.BASE_PATH, self.ARCHIVE_PATH) 
+
+        
+    def copy_file_from_archive(self):
+        """copy files from archive folder to input"""
+        if not os.path.exists(self.BASE_PATH+ "input"):
+            os.makedirs(self.BASE_PATH + "input")
+
+        elif not os.path.exists(self.ARCHIVE_PATH):
+            os.makedirs(self.BASE_PATH + "archive")
+
+        elif Path(self.ARCHIVE_PATH).glob("*.csv"):
+            with os.scandir(self.ARCHIVE_PATH) as files:
+                for file in files:
+                    shutil.copy(file,f"{self.BASE_PATH}/input")
+        else:
+            raise Exception("file not there in archive!!!")
+
+
+    # def copy_file_from_archive(self):
+    #         if not self.read_csv_file_if_exists():
+    #             shutil.copy(self.ARCHIVE_PATH,f"{self.BASE_PATH}/input")
+    #             return True
+    #         else:
+    #             raise Exception("file not there")
+
+    def merge_source_data(self, item, feature_name, **kwargs):    
         """get source data with a target feature"""        
         df_item = pd.read_csv(item, engine="c", sep=',')    
         if item.name == kwargs.get("file_name_1"): # say "first_data.csv"                    
@@ -43,7 +74,7 @@ class LoadData(Configuration):
 
     def load_csv_from_base_path(self):
         """load csv file from path"""
-        if self.read_csv_file_if_exists(): # file exists                
+        if self.read_csv_file_if_exists(): # file exists              
             with os.scandir(f"{self.BASE_PATH}/input") as files_in_basepath:
                 print(f"{self.BASE_PATH}/input")
                 data = pd.DataFrame() 
@@ -52,7 +83,7 @@ class LoadData(Configuration):
                         if Path(f"{self.BASE_PATH}/input/merged_input.csv").exists():
                             data = pd.read_csv(f"{self.BASE_PATH}/input/merged_input.csv")
                         else:
-                            df =  self.get_source_data(item,
+                            df =  self.merge_source_data(item,
                                                   'is_red',
                                                    file_name_1 = "white_wine.csv",
                                                    file_name_2 ="red_wine.csv",)
