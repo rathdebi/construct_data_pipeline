97ae403a66605c9d5f2e00b71b674bbef3508577
Debi Prasad Rath <debi.rath817@gmail.com>
2023-04-07 20:40:16 +0530

    load data issues fixed

--- data_loader/constants.py
+++ data_loader/constants.py
@@ -1,4 +1,4 @@
-# path setting 
+# source path settings
 
 BASE_PATH = "C:/Users/2000080516/OneDrive - Hexaware Technologies/Desktop/construct_data_pipeline"
 ARCHIVE_PATH = "C:/Users/2000080516/OneDrive - Hexaware Technologies/Desktop/construct_data_pipeline/archive"
--- data_loader/load_data.py
+++ data_loader/load_data.py
@@ -11,37 +11,30 @@ class LoadData(Configuration):
     def __init__(self,BASE_PATH, ARCHIVE_PATH):
         super().__init__(BASE_PATH, ARCHIVE_PATH)
 
-    # def read_csv_file_if_exists(self):
-    #     """reading CSV file if exist in a given path"""        
-    #     if Path(f"{self.BASE_PATH}/input").exists():
-    #         with os.scandir(f"{self.BASE_PATH}/input") as files_in_basepath:        
-    #             for item in files_in_basepath: 
-    #                 return True   
-    #     else:
-    #         return self.copy_file_from_archive() # copy files from archive folder
 
     def read_csv_file_if_exists(self):
         """reading CSV file if exists in a given path"""
-        if not os.path.exists(self.BASE_PATH + "input"):
-            os.makedirs(self.BASE_PATH + "input")
+        if not Path(f"{self.BASE_PATH}/input").exists():
+            os.makedirs(self.BASE_PATH+ f"""/input""")
    
-        elif not os.path.exists(self.ARCHIVE_PATH):
-            os.makedirs(self.BASE_PATH + "archive")
+        elif not Path(self.ARCHIVE_PATH).exists():
+            os.makedirs(self.BASE_PATH+ f"""/archive""")
 
-        elif not len(list(Path(f"{self.BASE_PATH}/input").glob("*.csv"))) == 2:
-            return self.copy_file_from_archive(self.BASE_PATH, self.ARCHIVE_PATH) 
+        elif not len(list(Path(f"{self.BASE_PATH}/input").glob("*.csv"))) >= 2:
+            self.copy_file_from_archive() 
    
         else:
-            return "input file exists!!!"
+            pass
+        return True
 
         
     def copy_file_from_archive(self):
         """copy files from archive folder to input"""
-        if not os.path.exists(self.BASE_PATH + "input"):
-            os.makedirs(self.BASE_PATH + "input")
+        if not Path(f"{self.BASE_PATH}/input").exists():
+            os.makedirs(self.BASE_PATH + f"""/input""")
 
-        elif not os.path.exists(self.ARCHIVE_PATH):
-            os.makedirs(self.BASE_PATH + "archive")
+        elif not Path(self.ARCHIVE_PATH):
+            os.makedirs(self.BASE_PATH + f"""archive""")
 
         elif len(list(Path(self.ARCHIVE_PATH).glob("*.csv"))) == 2:
             with os.scandir(self.ARCHIVE_PATH) as files:
@@ -49,49 +42,43 @@ class LoadData(Configuration):
                     shutil.copy(file,f"{self.BASE_PATH}/input")
         else:
             raise Exception("archive folder do not contain files!!!")
-
-
-
-    # def copy_file_from_archive(self):
-    #         if not self.read_csv_file_if_exists():
-    #             shutil.copy(self.ARCHIVE_PATH,f"{self.BASE_PATH}/input")
-    #             return True
-    #         else:
-    #             raise Exception("file not there")
+        
 
     def merge_source_data(self, item, feature_name, **kwargs):    
         """get source data with a target feature"""        
-        df_item = pd.read_csv(item, engine="c", sep=',')    
+        df_item = pd.read_csv(item, engine="c", sep=',')
         if item.name == kwargs.get("file_name_1"): # say "first_data.csv"                    
             df_item[feature_name] = 0
-    
         elif item.name == kwargs.get("file_name_2"): # say "second_data.csv"                    
-            df_item[feature_name] = 1  
-          
+            df_item[feature_name] = 1
+
         else:  
-            raise Exception("item do not exists")
+            raise Exception("item do not exists!!!")
         
         return df_item
 
     def load_csv_from_base_path(self):
         """load csv file from path"""
-        if self.read_csv_file_if_exists(): # file exists              
+        if self.read_csv_file_if_exists(): # file exists
+            print(self.read_csv_file_if_exists())           
             with os.scandir(f"{self.BASE_PATH}/input") as files_in_basepath:
-                print(f"{self.BASE_PATH}/input")
                 data = pd.DataFrame() 
                 for item in files_in_basepath:
+                    print("item in this iteration:::", item)
                     if item.name.endswith('.csv'):
                         if Path(f"{self.BASE_PATH}/input/merged_input_data.csv").exists():
-                            data = pd.read_csv(f"{self.BASE_PATH}/input/merged_input_data.csv")
+                            print("MERGED FILE PRESENT:::WE are all set!!!")
+                            return pd.read_csv(f"{self.BASE_PATH}/input/merged_input_data.csv")
                         else:
+                            print("MERGED FILE NOT PRESENT:::WE are in process to merge sources!!!")
                             df =  self.merge_source_data(item,
                                                   'is_red',
                                                    file_name_1 = "white_wine.csv",
                                                    file_name_2 ="red_wine.csv",)
-                            data = pd.concat([data,df],axis=0, ignore_index=True)
-                data.to_csv(f"{self.BASE_PATH}/input/merged_input_data.csv",index_label="sl_no")
-                return data            
+                            data = pd.concat([data,df],axis=0) # index based
+                data.to_csv(f"{self.BASE_PATH}/input/merged_input_data.csv",index=False)
+                return data.sample(frac=1).reset_index(drop=True)          
         else:
-            raise Exception("file can not be loaded!!!")
+            raise Exception("reading csv files from path got an exception!!!")
     
     
